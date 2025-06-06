1053aca5da29f62e040315d270277b7b74be8fda
Yahoshuva <pasupuletiyahoshuva1999@gmail.com>
2025-06-06 16:22:24 +0530

    fourth commit

--- src/test/java/com/jenkins/JenkinsegApplicationTests.java
+++ src/test/java/com/jenkins/JenkinsegApplicationTests.java
@@ -14,7 +14,7 @@ class JenkinsegApplicationTests {
 
 
 	@Test
-	void1 contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing..");
 		logger.info("Test case executing second...");
 		assertEquals(true,true);
